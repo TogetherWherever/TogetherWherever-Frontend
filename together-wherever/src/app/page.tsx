708e9047a738563b6101ec1ac8bddf5d0685236d
Tanabodee-Y <tanabodee.y@ku.th>
2025-01-12 02:29:28 +0700

    Implemented Home page.

--- together-wherever/src/app/page.tsx
+++ together-wherever/src/app/page.tsx
@@ -1,7 +1,63 @@
+'use client';
+
+import { BaseButton } from "@/app/components/buttons/BaseButton";
+import { TripCard } from "@/app/components/cards/TripCard"
+import { ReviewCard } from "@/app/components/cards/ReviewCard"
+
+const handleClick = () => {
+alert("Click!");
+};
+
+const start = new Date(2024, 2, 13);  // March 30, 2024
+const end = new Date(2024, 2, 15);    // March 31, 2024
+
 export default function Home() {
     return (
-        <div>
-            Home page
+        <div className="flex flex-col py-2">
+            <div>
+                <div className="flex justify-between items-center">
+                    <div className="text-extrabold text-4xl mb-4"> Recently viewed </div>
+                    <BaseButton 
+                        className="bg-moonstone-blue"
+                        color="earth-yellow"
+                        buttonTxt="New Trip" 
+                        leftIcon="/plus-icon.svg"
+                        onClick={handleClick}
+                    />
+                </div>
+                <div className="flex gap-8">
+                    <TripCard 
+                        cardName="Trip to Chaing Mai"
+                        profileImage="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+                        image="https://d3h1lg3ksw6i6b.cloudfront.net/media/image/2023/09/13/4969e4b8a93c41f6b1f2d161eaade88f_Chiang_Mai_Where_To_Eat_Drink_Stay_And_Play_In_The_Cultural_Capital_Of_Northern_Thaila.jpg"
+                        startDate={start}
+                        endDate={end}
+                        destinationsNumber={9}
+                        onClick={handleClick}
+                    />
+                    <TripCard 
+                        cardName="Trip to Chaing Mai"
+                        profileImage="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+                        image="https://d3h1lg3ksw6i6b.cloudfront.net/media/image/2023/09/13/4969e4b8a93c41f6b1f2d161eaade88f_Chiang_Mai_Where_To_Eat_Drink_Stay_And_Play_In_The_Cultural_Capital_Of_Northern_Thaila.jpg"
+                        startDate={start}
+                        endDate={end}
+                        destinationsNumber={9}
+                        onClick={handleClick}
+                    />
+                </div>
+            </div>
+            <div className="mt-8">
+                <div className="text-extrabold text-4xl mb-4"> Recently viewed </div>
+                <ReviewCard
+                    cardName="Phuket, Thailand."
+                    image="https://ik.imgkit.net/3vlqs5axxjf/external/ik-seo/http://images.ntmllc.com/v4/destination/Thailand/Phuket-City/220668_SCN_Phuket_iStock910551026_Z20B18/Phuket-City-Scenery.jpg?tr=w-780%2Ch-437%2Cfo-auto"
+                    profileImage="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+                    userName="Jane Doe"
+                    desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia dolor in augue auctor, at euismod nisl placerat. Fusce ac erat sed felis consequat tempus."
+                    onClick={handleClick}
+                />
+            </div>
+            
         </div>
     );
 }
