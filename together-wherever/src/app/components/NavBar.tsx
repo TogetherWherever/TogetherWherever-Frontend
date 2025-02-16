b11b7c04a5d40ca11c51d072b3147f27361ef7e5
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2025-02-17 06:18:17 +0700

    refactor: remove console.log and reformated file

--- together-wherever/src/app/components/NavBar.tsx
+++ together-wherever/src/app/components/NavBar.tsx
@@ -1,22 +1,22 @@
 'use client'; // Mark this as a Client Component
 
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { useEffect, useState } from "react";
+import {usePathname} from 'next/navigation';
+import {useEffect, useState} from "react";
 import Image from 'next/image';
-import { useRouter } from "next/navigation";
-import { BaseButton } from "@/app/components/buttons/BaseButton";
-import { jwtDecode } from 'jwt-decode';
+import {useRouter} from "next/navigation";
+import {BaseButton} from "@/app/components/buttons/BaseButton";
+import {jwtDecode} from 'jwt-decode';
 
 export default function NavBar() {
     const pathname = usePathname();
     const router = useRouter();
     const [username, setUsername] = useState<string | undefined>('');
-    
+
     const links = [
-        { href: '/', label: 'Home' },
-        { href: '/discover', label: 'Discover' },
-        { href: '/your-trips', label: 'Your Trips' },
+        {href: '/', label: 'Home'},
+        {href: '/discover', label: 'Discover'},
+        {href: '/your-trips', label: 'Your Trips'},
     ];
 
     const handleNavigateToSignInPage = () => {
@@ -30,12 +30,11 @@ export default function NavBar() {
     };
 
 
-    useEffect(() => {        
+    useEffect(() => {
         const token = localStorage.getItem('token');
         if (token) {
             try {
                 const decoded = jwtDecode(token);
-                console.log('Decoded token:', decoded);
                 setUsername(decoded.sub);
             } catch (error) {
                 console.error('Error decoding token:', error);
@@ -52,51 +51,50 @@ export default function NavBar() {
         };
 
         window.addEventListener("storage", handleStorageChange);
-        return () => window.removeEventListener("storage", handleStorageChange);    
+        return () => window.removeEventListener("storage", handleStorageChange);
     }, [pathname]);
 
     return (
         <nav className="flex items-center px-10 py-4 shadow-md z-40">
-                <Image src="/logo.png" alt="Logo" width={1000} height={1000} className="h-10 w-auto pr-4" />
+            <Image src="/logo.png" alt="Logo" width={1000} height={1000} className="h-10 w-auto pr-4"/>
             {links.map((link) => (
-                <Link 
-                    href={link.href} 
-                    key={link.label} 
+                <Link
+                    href={link.href}
+                    key={link.label}
                     className={`px-4 text-lg ${(() => {
-                        const isExactMatch = pathname === link.href;                        
+                        const isExactMatch = pathname === link.href;
                         const isHighlightedPath = () => {
                             if (pathname.startsWith('/home')) {
                                 return '/'
-                            } 
-                            else if (pathname.startsWith('/discover')) { 
+                            } else if (pathname.startsWith('/discover')) {
                                 return '/discover'
                             } else {
                                 return '/your-trips'
                             }
                         }
                         const isCreatingNewTrip = pathname.includes("/create-new-trip");
-                    
+
                         return isExactMatch || isCreatingNewTrip && isHighlightedPath() === link.href
-                          ? "text-earth-yellow font-bold"
-                          : "";
+                            ? "text-earth-yellow font-bold"
+                            : "";
                     })()}`}>
                     {link.label}
                 </Link>
             ))}
             {username ? (
-                <div 
+                <div
                     className="flex flex-row ml-auto text-lg font-semibold text-asparagus-green cursor-pointer"
                     onClick={handleNavigateToProfilePage}
                 >
-                    <p className='mr-1'>Hi!,</p><p>{ username }</p>
+                    <p className='mr-1'>Hi!,</p><p>{username}</p>
                 </div>
             ) : (
                 <div className='flex flex-row ml-auto'>
-                  <BaseButton 
-                        buttonTxt="Sign in" 
+                    <BaseButton
+                        buttonTxt="Sign in"
                         onClick={handleNavigateToSignInPage}
                         className='!text-base !py-2 !px-4'
-                    />  
+                    />
                 </div>
             )}
         </nav>
