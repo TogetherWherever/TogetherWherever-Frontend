3ddaf25f6a686d78e7e2dbeac8e388e867385b66
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2025-02-17 16:19:18 +0700

    chore: change card size

--- together-wherever/src/app/components/cards/BaseCard.tsx
+++ together-wherever/src/app/components/cards/BaseCard.tsx
@@ -6,17 +6,19 @@ interface BaseCardPropsInterface {
     height?: number;
     cardName: string;
     onClick?: () => void;
+    className?: string;
 };
 
-export const BaseCard = ({ image, width = 400, height = 300, cardName, onClick }: BaseCardPropsInterface) => {
+export const BaseCard = ({ image, width = 400, height = 300, cardName, onClick, className = "rounded-2xl" }: BaseCardPropsInterface) => {
     return (
-        <div className="relative cursor-pointer h-[300px]" onClick={onClick}>
+        <div className="relative cursor-pointer" onClick={onClick}>
             <Image
-                className="rounded-2xl"
+                className={className}
                 src={image}
                 alt="img"
                 width={width}
                 height={height}
+                style={{ objectFit: "cover" }}
             />
             <div className="w-full absolute bottom-0 left-0 px-2 py-3">
                 <h2 className="text-3xl text-mint-cream font-semibold">
