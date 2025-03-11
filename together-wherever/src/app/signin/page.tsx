8545fff8a7b45ef72b27f30c074117a9981d53f8
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2025-03-12 00:20:18 +0700

    chore: add show/hide password icon

--- together-wherever/src/app/signin/page.tsx
+++ together-wherever/src/app/signin/page.tsx
@@ -2,9 +2,11 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { EyeIcon, EyeSlashIcon }  from '@heroicons/react/24/outline'
 
 export default function AuthPage() {
     const [isLogin, setIsLogin] = useState(true);
+    const [showPassword, setShowPassword] = useState(false);
     const [formData, setFormData] = useState({
         username: '',
         password: '',
@@ -15,6 +17,7 @@ export default function AuthPage() {
     const router = useRouter();
 
     const toggleForm = () => setIsLogin(!isLogin);
+    const togglePassword = () => setShowPassword(!showPassword);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
@@ -63,7 +66,13 @@ export default function AuthPage() {
                         <input type="text" name="first_name" placeholder="First Name" onChange={handleChange} required className="w-full p-2 border rounded-lg focus:outline-moonstone-blue" />
                         <input type="text" name="last_name" placeholder="Last Name" onChange={handleChange} required className="w-full p-2 border rounded-lg focus:outline-moonstone-blue" />
                     </>}
-                    <input type="password" name="password" placeholder="Password" onChange={handleChange} required className="w-full p-2 border rounded-lg focus:outline-moonstone-blue" />
+                    {/* Password Field with Toggle Button */}
+                    <div className="relative w-full">
+                        <input type={showPassword ? "text" : "password"} name="password" placeholder="Password" onChange={handleChange} required className="w-full p-2 border rounded-lg focus:outline-moonstone-blue" />
+                        <button type="button" onClick={togglePassword} className="absolute inset-y-0 right-2 flex items-center text-gray-500">
+                            {showPassword ? <EyeSlashIcon className="h-5 w-5" /> : <EyeIcon className="h-5 w-5" />}
+                        </button>
+                    </div>
                     <button type="submit" className="w-full bg-moonstone-blue text-white p-2 rounded-xl hover:bg-teal-blue transition">
                         {isLogin ? 'Login' : 'Register'}
                     </button>
