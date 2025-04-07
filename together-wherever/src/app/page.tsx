049c2f697ecd1fb682fa62b9acb26135908a0e83
Tanabodee-Y <tanabodee.y@ku.th>
2025-04-08 04:16:46 +0700

    Add new responsive layout for home page.

--- together-wherever/src/app/page.tsx
+++ together-wherever/src/app/page.tsx
@@ -19,12 +19,12 @@ export default function Home() {
     if (loading) {
         return (
             <div className="fixed inset-0 flex items-center justify-center">
-                <ClipLoader size={50} color={"#60993E"} loading={loading}/>
+                <ClipLoader size={50} color={"#60993E"} loading={loading} />
             </div>
         );
     } else if (error) {
         <div className="fixed inset-0 flex items-center justify-center">
-            <ErrorReport/>
+            <ErrorReport />
         </div>
     }
 
@@ -42,7 +42,7 @@ export default function Home() {
                 <div className="flex flex-col md:flex-row w-full mt-auto">
                     <div className="flex items-center flex-col gap-6 w-full pr-4">
                         <div className="flex items-center flex-col gap-6">
-                            <MapPinIcon className="lg:w-[100px] w-[50px] text-earth-yellow"/>
+                            <MapPinIcon className="lg:w-[100px] w-[50px] text-earth-yellow" />
                             <BaseButton
                                 className="bg-asparagus-green !text-lg"
                                 color="earth-yellow"
@@ -60,7 +60,7 @@ export default function Home() {
 
                     <div className="flex justify-center items-center flex-col gap-6 w-full">
                         <div className="flex items-center flex-col gap-6">
-                            <GlobeAsiaAustraliaIcon className="lg:w-[100px] w-[50px] text-earth-yellow"/>
+                            <GlobeAsiaAustraliaIcon className="lg:w-[100px] w-[50px] text-earth-yellow" />
                             <BaseButton
                                 className="bg-asparagus-green !text-lg"
                                 color="earth-yellow"
@@ -81,44 +81,51 @@ export default function Home() {
     }
 
     return (
-        <div className="lg:px-[150px] sm:px-[75px] px-[50px] pt-[50px] pb-6">
-            <div className="flex flex-col py-2">
-                <div className="lg:flex flex-row justify-between items-center">
-                    <div className="flex flex-col gap-6">
-                        <label className="lg:text-5xl text-3xl font-bold text-asparagus-green">Plan Your Perfect
-                            Trip</label>
-                        <label className="lg:text-2xl text-xl">
-                            Whether traveling with friends or on your own, your next adventure starts here!
-                        </label>
-                    </div>
-                    <BaseButton
-                        className="bg-moonstone-blue mt-5 lg:mt-0 text-xl"
-                        color="earth-yellow"
-                        buttonTxt="New Trip"
-                        leftIcon={PlusIcon}
-                        leftIconCustomization="w-[25px] h-[25px]"
-                        onClick={handleNavigateTripPlanningPage}
-                    />
-                </div>
-                <div className="font-bold lg:text-4xl text-2xl lg:mt-12 mt-8 mb-4">Recently viewed</div>
-                <div className="grid sm:grid-cols-2 xl:grid-cols-3 grid-cols-1 gap-x-2 mt-2 justify-items-center">
-                    {recentlyViewData?.map((item: RecentlyViewData, index: number) => (
-                        <div
-                            key={item.viewTripId}
-                            className={`w-full pt-2 lg:pt-0 justify-self-start`}
-                        >
-                            <TripCard
-                                tripId={item.viewTripId}
-                                owner={item.username}
-                                cardName={item.tripName}
-                                image={item.photo}
-                                startDate={new Date(item.startDate)}
-                                endDate={new Date(item.endDate)}
-                                destinationsNumber={item.destinationsNumber}
-                                onClick={() => router.push(`/planning/${item.viewTripId}`)}
-                            />
+        <div className="flex justify-center w-full px-[75px] px-[50px] pt-[50px] pb-6">
+            <div className="w-full max-w-[1620px]">
+                <div className="flex flex-col py-2">
+                    <div className="xl:flex flex-row justify-between items-center">
+                        <div className="flex flex-col gap-6">
+                            <label className="lg:text-5xl text-3xl font-bold text-asparagus-green">Plan Your Perfect
+                                Trip</label>
+                            <label className="lg:text-2xl text-xl">
+                                Whether traveling with friends or on your own, your next adventure starts here!
+                            </label>
                         </div>
-                    ))}
+                        <BaseButton
+                            className="bg-moonstone-blue mt-5 xl:mt-0 text-xl"
+                            color="earth-yellow"
+                            buttonTxt="New Trip"
+                            leftIcon={PlusIcon}
+                            leftIconCustomization="w-[25px] h-[25px]"
+                            onClick={handleNavigateTripPlanningPage}
+                        />
+                    </div>
+                    <div className="font-bold lg:text-4xl text-2xl lg:mt-12 mt-8 mb-4">Recently viewed</div>
+                    <div className="grid lg:grid-cols-2 xl:grid-cols-3 sm:grid-cols-1 gap-x-2 sm:gap-y-2 mt-2 justify-items-center">
+                        {recentlyViewData?.map((item: RecentlyViewData, index: number) => (
+                            <div
+                                key={item.viewTripId}
+                                className={`pt-2 lg:pt-0 ${index % 3 === 0
+                                    ? "justify-self-start"
+                                    : index % 3 === 1
+                                        ? "xl:justify-self-center justify-self-start"
+                                        : "xl:justify-self-end justify-self-start"
+                                    }`}
+                            >
+                                <TripCard
+                                    tripId={item.viewTripId}
+                                    owner={item.username}
+                                    cardName={item.tripName}
+                                    image={item.photo}
+                                    startDate={new Date(item.startDate)}
+                                    endDate={new Date(item.endDate)}
+                                    destinationsNumber={item.destinationsNumber}
+                                    onClick={() => router.push(`/planning/${item.viewTripId}`)}
+                                />
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
